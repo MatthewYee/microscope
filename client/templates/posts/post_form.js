5fbc7b4d597729462bf28a8d56e5776c74b81044
Matthew Yee <mgy@hawaii.edu>
2015-10-29 01:12:35 -1000

    update though need to move the whole three bar down

---
+++ client/templates/posts/post_form.js
@@ -0,0 +1,17 @@
+/**
+ * Created by MGY on 10/29/2015.
+ */
+Template.postForm.events({
+  // handle the form submission
+  'submit form': function(event, template) {
+
+    // stop the form from submitting
+    event.preventDefault();
+    Posts.insert({
+      title: event.target.url.value,
+      url: event.target.url.value
+    });
+    template.find("form").reset();
+  }
+
+});
